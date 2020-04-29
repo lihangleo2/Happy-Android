c5f29af8b71a006654b52b15777b8bcacb11f082
leo <lihangleo2@gmail.com>
2020-04-29 13:55:21 +0800

    增加异步初始化优化启动速度

--- app/src/main/java/com/lihang/selfmvvm/MyApplication.java
+++ app/src/main/java/com/lihang/selfmvvm/MyApplication.java
@@ -5,35 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 
 import com.lihang.selfmvvm.bean.User;
-import com.lihang.selfmvvm.customview.x5webview.X5InitService;
 import com.lihang.selfmvvm.launchstater.TaskDispatcher;
 import com.lihang.selfmvvm.launchstater.mytasks.SmartRefreshLayoutTask;
 import com.lihang.selfmvvm.launchstater.mytasks.X5WebTask;
 import com.lihang.selfmvvm.utils.PreferenceUtil;
-import com.scwang.smartrefresh.layout.SmartRefreshLayout;
-import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
-import com.scwang.smartrefresh.layout.header.ClassicsHeader;
 
 /**
  * Created by leo
  * on 2019/10/15.
  */
 public class MyApplication extends Application {
-
-//    //static 代码段可以防止内存泄露
-//    static {
-//        //设置全局的Header构建器
-//        SmartRefreshLayout.setDefaultRefreshHeaderCreator((mContext, layout) -> {
-////            layout.setPrimaryColorsId(R.color.white, R.color.status_background);//全局设置主题颜色
-//            return new ClassicsHeader(mContext).setPrimaryColorId(R.color.white).setAccentColorId(R.color.status_background);//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
-//        });
-//        //设置全局的Footer构建器
-//        SmartRefreshLayout.setDefaultRefreshFooterCreator((mContext, layout) -> {
-//            return new ClassicsFooter(mContext).setPrimaryColorId(R.color.white).setAccentColorId(R.color.status_background);
-//        });
-//
-//    }
-
 
     private static MyApplication context;
     private static User loginUser;
@@ -51,7 +32,6 @@ public class MyApplication extends Application {
                 .addTask(new X5WebTask())
                 .start();
 
-//        initX5web();
     }
 
     public static User getLoginUser() {
@@ -71,11 +51,6 @@ public class MyApplication extends Application {
         PreferenceUtil.clearByClass(User.class);
     }
 
-
-    private void initX5web() {
-//        Intent intent = new Intent(this, X5InitService.class);
-//        startService(intent);
-    }
 
     public static Context getContext() {
         return context;
