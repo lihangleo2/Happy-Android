da1f3fb4175e6dc156a8a456c9b39817e7d2b33a
leo <lihangleo2@163.com>
2020-06-17 14:39:58 +0800

    Update WelComeActivity.java

--- app/src/main/java/com/lihang/selfmvvm/ui/activity/WelComeActivity.java
+++ app/src/main/java/com/lihang/selfmvvm/ui/activity/WelComeActivity.java
@@ -38,7 +38,7 @@ public class WelComeActivity extends BaseActivity<NormalViewModel, ActivityWelco
         binding.imgSvg.setTraceColor(getResources().getColor(R.color.white));
         binding.imgSvg.start();
 
-        Observable.timer(2500, TimeUnit.MILLISECONDS).observeOn(AndroidSchedulers.mainThread()).subscribe(aLong -> {
+        Observable.timer(2500, TimeUnit.MILLISECONDS).observeOn(AndroidSchedulers.mainThread()).compose(bindToLifecycle()).subscribe(aLong -> {
             ActivityUtils.startActivity(WelComeActivity.this, HomeActivity.class);
             ActivityUtils.finishWithAnim(WelComeActivity.this,0,R.animator.set_anim_activity_exit);
         });
