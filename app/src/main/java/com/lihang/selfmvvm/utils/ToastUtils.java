7b94904d127e907a0bdc731a82ba662523c0855f
leo <lihangleo2@163.com>
2020-07-02 16:05:58 +0800

    Update ToastUtils.java

--- app/src/main/java/com/lihang/selfmvvm/utils/ToastUtils.java
+++ app/src/main/java/com/lihang/selfmvvm/utils/ToastUtils.java
@@ -13,37 +13,28 @@ import com.lihang.selfmvvm.MyApplication;
 
 public class ToastUtils {
     private static Context context = MyApplication.getContext();
-    /** 之前显示的内容 */
-    private static String oldMsg ;
-    /** Toast对象 */
-    private static Toast toast = null ;
-    /** 第一次时间 */
-    private static long oneTime = 0 ;
-    /** 第二次时间 */
-    private static long twoTime = 0 ;
+    /**
+     * Toast对象
+     */
+    private static Toast toast = null;
+
 
     /**
      * 显示Toast
+     *
      * @param message
      */
-    public static void showToast(String message){
-        if(toast == null){
-            toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
-            toast.show() ;
-            oneTime = System.currentTimeMillis() ;
-        }else{
-            twoTime = System.currentTimeMillis() ;
-            if(message.equals(oldMsg)){
-                if(twoTime - oneTime > Toast.LENGTH_SHORT){
-                    toast.show() ;
-                }
-            }else{
-                oldMsg = message ;
-                toast.setText(message) ;
-                toast.show() ;
-            }
+    public static void showToast(String message) {
+        if (ButtonClickUtils.isFastClick()) {
+            return;
         }
-        oneTime = twoTime ;
+        if (toast == null) {
+            toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            toast.cancel();
+            toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
-
 }
