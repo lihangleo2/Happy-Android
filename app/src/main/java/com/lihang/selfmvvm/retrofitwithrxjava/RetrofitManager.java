6fe85e7914c6624f0d217f0607434dd563d570d1
leo <lihangleo2@163.com>
2020-09-16 11:30:49 +0800

    Update RetrofitManager.java

--- app/src/main/java/com/lihang/selfmvvm/retrofitwithrxjava/RetrofitManager.java
+++ app/src/main/java/com/lihang/selfmvvm/retrofitwithrxjava/RetrofitManager.java
@@ -40,6 +40,7 @@ public class RetrofitManager {
 //        cookieJar.clear();
 //        cookieJar.clearSession();
         initOkHttpClient();
+        ignoreSSLCheck();
         initRetrofit();
     }
 
@@ -95,6 +96,54 @@ public class RetrofitManager {
                 .addNetworkInterceptor(NetCacheInterceptor.getInstance())
                 .cookieJar(cookieJar)
                 .build();
+    }
+    
+    
+    private void ignoreSSLCheck() {
+        //忽略ssl校验
+        SSLContext sc = null;
+        try {
+            sc = SSLContext.getInstance("SSL");
+            sc.init(null, new TrustManager[]{new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws java.security.cert.CertificateException {
+
+                }
+
+                @Override
+                public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws java.security.cert.CertificateException {
+
+                }
+
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            }}, new SecureRandom());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        HostnameVerifier hv1 = new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+
+        String workerClassName = "okhttp3.OkHttpClient";
+        try {
+            Class workerClass = Class.forName(workerClassName);
+            Field hostnameVerifier = workerClass.getDeclaredField("hostnameVerifier");
+            hostnameVerifier.setAccessible(true);
+            hostnameVerifier.set(okHttpClient, hv1);
+
+            Field sslSocketFactory = workerClass.getDeclaredField("sslSocketFactory");
+            sslSocketFactory.setAccessible(true);
+            sslSocketFactory.set(okHttpClient, sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
