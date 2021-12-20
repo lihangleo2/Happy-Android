021464d3c6a01909e4698e17059b4bf4c28b9e86
leo <lihangleo2@163.com>
2021-12-20 11:50:13 +0800

    Update MyApplication.java

--- app/src/main/java/com/lihang/selfmvvm/MyApplication.java
+++ app/src/main/java/com/lihang/selfmvvm/MyApplication.java
@@ -73,38 +73,7 @@ public class MyApplication extends Application {
         return context;
     }
     
-    
-    
-        public  void handleSSLHandshake() {
-        //解决三星(部分手机)，glide加载https，加载不除来
-        try {
-            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
-                public X509Certificate[] getAcceptedIssuers() {
-                    return new X509Certificate[0];
-                }
-                @Override
-                public void checkClientTrusted(X509Certificate[] certs, String authType) {
-
-                }
-                @Override
-                public void checkServerTrusted(X509Certificate[] certs, String authType) {
-
-                }
-            }};
-            SSLContext sc = SSLContext.getInstance("TLS");
-            // trustAllCerts信任所有的证书
-            sc.init(null, trustAllCerts, new SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
-                @Override
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                }
-            });
-        } catch (Exception ignored) {
-
-        }
-    }
+   
     
     
         public  void handleSSLHandshake() {
